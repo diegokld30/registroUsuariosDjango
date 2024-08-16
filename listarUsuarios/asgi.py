f92cafa6e23d56d5993d9680fc5acd8debcbb1cb
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-16 08:23:00 -0500

    Cargando mi proyecto django con una app llamada user

---
+++ listarUsuarios/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for listarUsuarios project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'listarUsuarios.settings')
+
+application = get_asgi_application()
