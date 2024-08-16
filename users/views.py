41b7620202e69fc0f3a5bb0731299eb8bfa0a320
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-16 11:27:11 -0500

    Creando la vista y el template para registrar usuarios

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Persona
+from .form import PersonaForm
 
 # Create your views here.
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarUsuario')
+    else:
+        form = PersonaForm()
+
+    return render(request, 'registroUsuario.html', {'form': form})
+
